4a53ec5cc4cc58871f0e185c3d719cc0c85c5a55
student <katsadzeoto@gmail.com>
2025-04-22 10:49:49 +0400

    Initial commit: ASP.NET MVC project with greeting form

---
+++ Controllers/GreetController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevOpsMidterm.Controllers
+{
+    public class GreetController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Greet(string name)
+        {
+            ViewBag.Name = name;
+            return View();
+        }
+    }
+}
